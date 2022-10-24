80167024ebdfd619aa28309e7746cbcb22fe2c4b
emiryayci <96650218+emiryayci@users.noreply.github.com>
2022-10-24 22:54:25 +0300

    Add files via upload

--- sinavNotu.py
+++ sinavNotu.py
@@ -1,12 +1,25 @@
 #Sınav notunu hangi harfle geçtiğini hesaplayan program
-sinavNotu=int(input("Sınav notunuz :"))
-if sinavNotu<50:
+vizeNotu=int(input("Vize notunuz :"))
+finalNotu=int(input("Final notunuz :"))
+if vizeNotu<50:
     print("Sınav notunuz :F (GEÇEMEDİNİZ)")
-elif 64>sinavNotu>=50:
-    print("Sınav notunuz DD ( Sorumlu Geçtiniz)")
-elif 74>sinavNotu>65:
-    print("Sınav notunuz CC (Geçtiniz)")
-elif 89>sinavNotu>75:
-    print("Sınav notunuz BB (Geçtiniz)")
-elif 100>=sinavNotu>90:
-    print("Sınav notunuz AA (Geçtiniz)")
+if 64>vizeNotu>=50:
+    print("Sınav notunuz :D ( Sorumlu Geçtiniz)")
+if 74>vizeNotu>65:
+    print("Sınav notunuz :C (Geçtiniz)")
+if 89>vizeNotu>75:
+    print("Sınav notunuz :B (Geçtiniz)")
+if 100>=vizeNotu>90:
+    print("Sınav notunuz :A (Geçtiniz)")
+if finalNotu<35:
+        print("Sınav notunuz: F (Geçemediniz)")
+if 36<finalNotu<49:
+    print("Sınav notunuz :E (Şartlı geçtiniz)")
+if 50<finalNotu<64:
+    print("Sınav notunuz :D (Geçtiniz)")
+if 65<finalNotu<74:
+    print("Sınav notunuz :C (Geçtiniz)")
+if 75<finalNotu<84:
+    print("Sınav notunuz :B (Geçtiniz)")
+if 85<finalNotu<=100:
+    print("Sınav notunuz :A (Geçtiniz)")
